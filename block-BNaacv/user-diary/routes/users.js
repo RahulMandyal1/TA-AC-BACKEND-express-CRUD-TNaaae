1ab752d25dcbd429792e2fdb1cae554523a043eb
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-03-25 10:13:06 +0530

    Updated First Assignment  express

--- block-BNaacv/user-diary/routes/users.js
+++ block-BNaacv/user-diary/routes/users.js
@@ -4,37 +4,21 @@ let router = express.Router();
 router.get('/',(req ,res)=>{
     res.render('users',{ list: ["ankit", "suraj", "prashant", "ravi"] });
 })
-// display a single user here .....
-router.get('/:id',(req ,res)=>{
-    // let id = req.params.id;
-    //  if we have database then we are going to find like this :
-    //  modelname.findById(id, (err , userData)=>{
-    //            res.json(usedata);
-    //  })
-    //  res.send('Delete the use successfully from the database');
-    res.render('singleUser',{ name: "rahul", email: "rahul@altcampus.io" });
-})
 // Create a new user  here we have to display a user form 
 router.get('/new',(req ,res)=>{
-
     res.render('userForm');
 })
+// display a single user here .....
+router.get('/:id',(req ,res)=>{
+    res.render('singleUser',{ name: "rahul", email: "rahul@altcampus.io" });
+})
+
 
 router.delete('/:id',(req ,res)=>{
-     let id = req.params.id;
-    //  if we have database then we are going to delete :
-    //  modelname.delete(id, (err , deletedDocumentData)=>{
-    //        res.send('user is deleted successfully');
-    //  })
-     res.send('Delete the use successfully from the database');
+    //   if we have database connectivity then we  can delete the single user data 
 })
 // get a query from the  url and updates  the  data 
 router.put('/:id',(req ,res)=>{
-    let id = req.params.id;
-    //  if we have database then we are going to update:
-    //  modelname.findAndUpdateById(id,{new : true}, (err , updatedUser)=>{
-    //       res.send(udpatedUser.name+ 'is udated Sucessfully');
-    //  })
-     res.send('Delete the use successfully from the database');
+    // if we have  the database connectivity then we can update the data 
 })
 module.exports = router;
